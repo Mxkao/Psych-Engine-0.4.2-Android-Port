37c7501727872675c3db2ba5b9398c0d33cf30ea
Mxkao <99405850+Mxkao@users.noreply.github.com>
2022-02-10 10:00:16 -0300

    Add files via upload

--- assets/preload/custom_events/Opponent Fade.lua
+++ assets/preload/custom_events/Opponent Fade.lua
@@ -2,18 +2,14 @@
 function onEvent(name, value1, value2)
 	if name == 'Opponent Fade' then
 		duration = tonumber(value1);
-		if duration < 0 then
-			duration = 0;
+		if duration > 0 then
+			makeLuaSprite('overlays', 'overlay', -800, -300)
+			addLuaSprite('overlays', true)
+			setBlendMode('overlays','multiply');
+			setGraphicSize('overlays', 0.7, 0.7)
+			setScrollFactor('overlays',0,0)
 		end
 
-		targetAlpha = tonumber(value2);
-		if duration == 0 then
-			setProperty('dad.alpha', targetAlpha);
-			setProperty('iconP2.alpha', targetAlpha);
-		else
-			doTweenAlpha('dadFadeEventTween', 'dad', targetAlpha, duration, 'linear');
-			doTweenAlpha('iconDadFadeEventTween', 'iconP2', targetAlpha, duration, 'linear');
-		end
-		--debugPrint('Event triggered: ', name, duration, targetAlpha);
+	
 	end
 end
